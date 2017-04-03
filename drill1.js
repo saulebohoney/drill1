ca0a7c37e365b2fd3fcb16ca5fdfe7c0aeec4543
htwright <harrisontwright@gmail.com>
2017-04-03 13:01:38 -0500

    almost-final commit

--- drill1.js
+++ drill1.js
@@ -1,24 +1,29 @@
-function whoAmi(name, age){
-	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-}
-
+function whoAmI(name, age){
+	if (typeof name !== 'string' || age < 0 || typeof age !== 'number'){
+	throw new Error("invalid input for whoAmI");
+} else{
+		console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+	}
+	
 
 
 function yearOfBirth(age){
-	try{
-		if (age < 0){
-			throw new Error("Age can not be negative");
-		} else {
-			const yearOfBirth = 2017-age;
-			console.log(" I was born in " + yearOfBirth);
-		}
-	}
-	catch(err){
-		console.log(err.message);
+	if (age < 0 || typeof age !== 'number') {
+		throw new Error("Age must be a positive intiger");
+	} else{
+		const yearOfBirth = 2017-age;
+		console.log(" I was born in " + yearOfBirth);
 
 	}
+	
+
 }
 
-
-whoAmi('name','34');
+try {
+whoAmI('name',34);
 yearOfBirth(34);
+}
+catch(err){
+	console.log(err.message);
+
+}
