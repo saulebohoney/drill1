c772c20c966bb0f052f31d162978e55b7b19427b
htwright <harrisontwright@gmail.com>
2017-04-03 11:28:43 -0500

    final commit of drill1

--- drill1.js
+++ drill1.js
@@ -1,12 +1,24 @@
-function whoAmi(name, age)
-{
-
-console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-
+function whoAmi(name, age){
+	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 }
+
+
+
 function yearOfBirth(age){
-	const yearOfBirth = 2017-age;
-	console.log(" I was born in " + yearOfBirth);
+	try{
+		if (age < 0){
+			throw new Error("Age can not be negative");
+		} else {
+			const yearOfBirth = 2017-age;
+			console.log(" I was born in " + yearOfBirth);
+		}
+	}
+	catch(err){
+		console.log(err.message);
+
+	}
 }
+
+
 whoAmi('name','34');
 yearOfBirth(34);
