39408b6d5a13653a32824d789fe1cd35d18553a9
htwright <harrisontwright@gmail.com>
2017-04-03 11:02:38 -0500

    added age

--- drill1.js
+++ drill1.js
@@ -1,5 +1,7 @@
 function whoAmi(name, age)
 {
+const yearOfBirth = 2017-age;
 console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+console.log(" I was born in " + yearOfBirth);
 }
 whoAmi('name','34');
